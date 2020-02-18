e7c3299f290ff121cbda4a5d017dc6b353541040
xiaojunnuo <25299493@qq.com>
2020-02-19 00:52:38 +0800

    fix: 修复获取用户角色出错的bug

--- sql/d2p_pm.sql
+++ sql/d2p_pm.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50721
-Source Host           : localhost:3306
+Source Server         : 我的阿里云
+Source Server Version : 50724
+Source Host           : veryreader.com:13306
 Source Database       : d2p_pm
 
 Target Server Type    : MYSQL
-Target Server Version : 50721
+Target Server Version : 50724
 File Encoding         : 65001
 
-Date: 2020-02-18 20:08:36
+Date: 2020-02-19 00:51:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -39,12 +39,12 @@ CREATE TABLE `pm_resource` (
 -- ----------------------------
 -- Records of pm_resource
 -- ----------------------------
-INSERT INTO `pm_resource` VALUES ('1', 'aside', '侧边菜单', null, null, null, null, '100', '1', null, '0', null, null);
+INSERT INTO `pm_resource` VALUES ('1', 'aside', '侧边菜单', null, null, null, null, '50', '1', null, '0', null, '2020-02-19 00:28:17');
 INSERT INTO `pm_resource` VALUES ('2', 'header', '顶部菜单', null, null, null, null, '100', '1', null, '0', null, null);
 INSERT INTO `pm_resource` VALUES ('7', 'permission', '权限管理', '', '', '', '', '1', '1', '1', '0', '2020-02-14 22:05:01', '2020-02-14 22:05:01');
 INSERT INTO `pm_resource` VALUES ('8', 'resource', '资源管理', 'permission:resource:view', '/permission/resource', '/permission/views/resource', 'address-book', '0', '1', '7', '0', '2020-02-14 22:13:00', '2020-02-14 22:13:00');
 INSERT INTO `pm_resource` VALUES ('9', 'role', '角色管理', 'permission:role:view', '/permission/role', '/permission/views/role', null, '1', '1', '7', '0', '2020-02-15 17:26:39', '2020-02-15 17:26:39');
-INSERT INTO `pm_resource` VALUES ('10', 'usersphere', '用户中心', null, null, null, null, '1', '1', '1', '0', '2020-02-15 18:46:44', '2020-02-15 18:46:44');
+INSERT INTO `pm_resource` VALUES ('10', 'usersphere', '用户中心', null, null, null, null, '100', '1', '1', '0', '2020-02-15 18:46:44', '2020-02-15 18:46:44');
 INSERT INTO `pm_resource` VALUES ('11', 'user', '用户管理', 'permission:user:view', '/usersphere/user', '/usersphere/views/user', null, null, '1', '10', '0', '2020-02-15 18:47:32', '2020-02-15 18:47:32');
 INSERT INTO `pm_resource` VALUES ('12', 'addResource', '添加资源', 'permission:resource:add', null, null, null, '100', '2', '8', '0', '2020-02-15 18:53:48', '2020-02-15 18:53:48');
 INSERT INTO `pm_resource` VALUES ('13', 'editResource', '修改资源', 'permission:resource:edit', null, null, null, '100', '2', '8', '0', '2020-02-15 18:54:18', '2020-02-15 18:54:18');
@@ -70,7 +70,7 @@ CREATE TABLE `pm_role` (
 -- Records of pm_role
 -- ----------------------------
 INSERT INTO `pm_role` VALUES ('1', '管理员', 'admin', '0', '0', '2020-02-15 17:36:18', '2020-02-15 17:36:18');
-INSERT INTO `pm_role` VALUES ('3', '菜单管理员', 'menuManager', null, '0', '2020-02-17 23:31:05', '2020-02-17 23:31:05');
+INSERT INTO `pm_role` VALUES ('3', '菜单管理员', 'menuManager', '0', '0', '2020-02-17 23:31:05', '2020-02-17 23:31:05');
 
 -- ----------------------------
 -- Table structure for pm_role_resource
@@ -84,7 +84,7 @@ CREATE TABLE `pm_role_resource` (
   `create_time` datetime NOT NULL,
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of pm_role_resource
@@ -92,8 +92,8 @@ CREATE TABLE `pm_role_resource` (
 INSERT INTO `pm_role_resource` VALUES ('1', '1', '7', '1', '2020-02-15 18:36:22', '2020-02-15 18:36:25');
 INSERT INTO `pm_role_resource` VALUES ('2', '1', '8', '1', '2020-02-15 18:36:53', '2020-02-15 18:36:55');
 INSERT INTO `pm_role_resource` VALUES ('3', '1', '3', '1', '2020-02-15 18:37:02', '2020-02-15 18:37:04');
-INSERT INTO `pm_role_resource` VALUES ('4', '1', '1', '0', '2020-02-15 19:41:47', '2020-02-15 19:41:47');
-INSERT INTO `pm_role_resource` VALUES ('5', '1', '2', '0', '2020-02-15 19:41:48', '2020-02-15 19:41:48');
+INSERT INTO `pm_role_resource` VALUES ('4', '1', '1', '1', '2020-02-15 19:41:47', '2020-02-15 19:41:47');
+INSERT INTO `pm_role_resource` VALUES ('5', '1', '2', '1', '2020-02-15 19:41:48', '2020-02-15 19:41:48');
 INSERT INTO `pm_role_resource` VALUES ('6', '1', '12', '1', '2020-02-15 19:41:48', '2020-02-15 19:41:48');
 INSERT INTO `pm_role_resource` VALUES ('7', '1', '13', '1', '2020-02-15 19:41:48', '2020-02-15 19:41:48');
 INSERT INTO `pm_role_resource` VALUES ('8', '1', '14', '1', '2020-02-15 19:41:48', '2020-02-15 19:41:48');
@@ -114,14 +114,14 @@ INSERT INTO `pm_role_resource` VALUES ('22', '1', '14', '1', '2020-02-15 19:43:1
 INSERT INTO `pm_role_resource` VALUES ('23', '1', '3', '1', '2020-02-15 19:43:10', '2020-02-15 19:43:10');
 INSERT INTO `pm_role_resource` VALUES ('24', '1', '10', '1', '2020-02-15 19:43:10', '2020-02-15 19:43:10');
 INSERT INTO `pm_role_resource` VALUES ('25', '1', '11', '1', '2020-02-15 19:43:10', '2020-02-15 19:43:10');
-INSERT INTO `pm_role_resource` VALUES ('26', '1', '7', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('27', '1', '8', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('28', '1', '12', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('29', '1', '13', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('30', '1', '14', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('31', '1', '9', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('32', '1', '10', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
-INSERT INTO `pm_role_resource` VALUES ('33', '1', '11', '0', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('26', '1', '7', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('27', '1', '8', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('28', '1', '12', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('29', '1', '13', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('30', '1', '14', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('31', '1', '9', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('32', '1', '10', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
+INSERT INTO `pm_role_resource` VALUES ('33', '1', '11', '1', '2020-02-15 19:43:23', '2020-02-15 19:43:23');
 INSERT INTO `pm_role_resource` VALUES ('34', '3', '7', '1', '2020-02-17 23:31:13', '2020-02-17 23:31:13');
 INSERT INTO `pm_role_resource` VALUES ('35', '3', '8', '1', '2020-02-17 23:31:13', '2020-02-17 23:31:13');
 INSERT INTO `pm_role_resource` VALUES ('36', '3', '12', '1', '2020-02-17 23:31:13', '2020-02-17 23:31:13');
@@ -137,14 +137,45 @@ INSERT INTO `pm_role_resource` VALUES ('45', '3', '14', '1', '2020-02-17 23:41:4
 INSERT INTO `pm_role_resource` VALUES ('46', '3', '9', '1', '2020-02-17 23:41:48', '2020-02-17 23:41:48');
 INSERT INTO `pm_role_resource` VALUES ('47', '3', '2', '1', '2020-02-17 23:41:48', '2020-02-17 23:41:48');
 INSERT INTO `pm_role_resource` VALUES ('48', '3', '1', '1', '2020-02-17 23:41:48', '2020-02-17 23:41:48');
-INSERT INTO `pm_role_resource` VALUES ('49', '3', '7', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('50', '3', '8', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('51', '3', '12', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('52', '3', '13', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('53', '3', '14', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('54', '3', '9', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('55', '3', '2', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
-INSERT INTO `pm_role_resource` VALUES ('56', '3', '1', '0', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('49', '3', '7', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('50', '3', '8', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('51', '3', '12', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('52', '3', '13', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('53', '3', '14', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('54', '3', '9', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('55', '3', '2', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('56', '3', '1', '1', '2020-02-17 23:42:09', '2020-02-17 23:42:09');
+INSERT INTO `pm_role_resource` VALUES ('57', '1', '1', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('58', '1', '7', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('59', '1', '8', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('60', '1', '15', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('61', '1', '12', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('62', '1', '13', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('63', '1', '14', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('64', '1', '9', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('65', '1', '10', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('66', '1', '11', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('67', '1', '2', '1', '2020-02-19 00:27:03', '2020-02-19 00:27:03');
+INSERT INTO `pm_role_resource` VALUES ('68', '1', '1', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('69', '1', '7', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('70', '1', '8', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('71', '1', '15', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('72', '1', '12', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('73', '1', '13', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('74', '1', '14', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('75', '1', '9', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('76', '1', '10', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('77', '1', '11', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('78', '1', '2', '0', '2020-02-19 00:27:20', '2020-02-19 00:27:20');
+INSERT INTO `pm_role_resource` VALUES ('79', '3', '7', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('80', '3', '8', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('81', '3', '15', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('82', '3', '12', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('83', '3', '13', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('84', '3', '14', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('85', '3', '9', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('86', '3', '2', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
+INSERT INTO `pm_role_resource` VALUES ('87', '3', '1', '0', '2020-02-19 00:27:32', '2020-02-19 00:27:32');
 
 -- ----------------------------
 -- Table structure for pm_user_role
@@ -158,14 +189,17 @@ CREATE TABLE `pm_user_role` (
   `create_time` datetime NOT NULL,
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COMMENT='用户角色';
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COMMENT='用户角色';
 
 -- ----------------------------
 -- Records of pm_user_role
 -- ----------------------------
-INSERT INTO `pm_user_role` VALUES ('4', '2', '3', '0', '2020-02-17 23:31:24', '2020-02-17 23:31:24');
-INSERT INTO `pm_user_role` VALUES ('5', '1', '1', '0', '2020-02-18 15:27:30', '2020-02-18 15:27:30');
-INSERT INTO `pm_user_role` VALUES ('6', '1', '3', '0', '2020-02-18 15:27:30', '2020-02-18 15:27:30');
+INSERT INTO `pm_user_role` VALUES ('4', '2', '3', '1', '2020-02-17 23:31:24', '2020-02-17 23:31:24');
+INSERT INTO `pm_user_role` VALUES ('5', '1', '1', '1', '2020-02-18 15:27:30', '2020-02-18 15:27:30');
+INSERT INTO `pm_user_role` VALUES ('6', '1', '3', '1', '2020-02-18 15:27:30', '2020-02-18 15:27:30');
+INSERT INTO `pm_user_role` VALUES ('7', '1', '1', '0', '2020-02-18 21:54:38', '2020-02-18 21:54:38');
+INSERT INTO `pm_user_role` VALUES ('8', '1', '3', '0', '2020-02-18 21:54:38', '2020-02-18 21:54:38');
+INSERT INTO `pm_user_role` VALUES ('9', '2', '3', '0', '2020-02-19 00:42:57', '2020-02-19 00:42:57');
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -190,5 +224,5 @@ CREATE TABLE `sys_user` (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'admin', '$2a$10$CvMJvWsWWl9miFggFjRhOuj3nQeUuhoW48M.SeAFCfYsp0lSmOscW', '管理员', 'https://d2p-demo-1251260344.cos.ap-guangzhou.myqcloud.com/file/2020/2/16/30428108036833.jpg', '1', '111@qq.com', '86', '18603046467', '0', null, '2020-02-15 20:49:15');
+INSERT INTO `sys_user` VALUES ('1', 'admin', '$2a$10$irJN827gfkpwdHJl9FbV4u71m9GN8b0qOsefpd1A22m2.PtQIvDaC', '管理员', 'https://d2p-demo-1251260344.cos.ap-guangzhou.myqcloud.com/file/2020/2/16/30428108036833.jpg', '1', '111@qq.com', '86', '18603046467', '0', '2020-02-18 21:55:02', '2020-02-15 20:49:15');
 INSERT INTO `sys_user` VALUES ('2', 'test', '$2a$10$tfeLZESgZYMTKcI5FEuij.pmDuDH1ThyqX/BIMW3/QQ46UeYapcBq', 'test', 'https://d2p-demo-1251260344.cos.ap-guangzhou.myqcloud.com/file/2020/2/16/94104782868855.jpg', '1', '2@qq.com', '86', '18603046467', '0', '2020-02-15 20:23:15', '2020-02-15 20:23:15');
