44c6446bb15ab3107830b41b887deeb752e2e667
xiaojunnuo@126.com <xiaojunnuo@qq.com>
2020-08-26 09:26:04 +0800

    fix: 修复用户没有添加按钮的bug

--- api-server/src/main/resources/db/migration/V1.3__user_permission.sql
+++ api-server/src/main/resources/db/migration/V1.3__user_permission.sql
@@ -5,7 +5,7 @@ INSERT INTO `pm_resource` VALUES ('29', '1', 'usersphere_user_add', '用户新�
 INSERT INTO `pm_resource` VALUES ('30', '1', 'usersphere_user_edit', '用户修改', 'usersphere:user:edit', null, null, null, '100', '2', '11', '0', '2020-02-28 13:27:39', '2020-02-28 13:27:39');
 INSERT INTO `pm_resource` VALUES ('31', '1', 'usersphere_user_del', '用户删除', 'usersphere:user:del', null, null, null, '100', '2', '11', '0', '2020-02-28 13:27:39', '2020-02-28 13:27:39');
 
-INSERT INTO `pm_role_resource` VALUES ('252', '1', '1', '28', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
-INSERT INTO `pm_role_resource` VALUES ('253', '1', '1', '29', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
-INSERT INTO `pm_role_resource` VALUES ('254', '1', '1', '30', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
-INSERT INTO `pm_role_resource` VALUES ('255', '1', '1', '31', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
+INSERT INTO `pm_role_resource` VALUES (null, '1', '1', '28', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
+INSERT INTO `pm_role_resource` VALUES (null, '1', '1', '29', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
+INSERT INTO `pm_role_resource` VALUES (null, '1', '1', '30', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
+INSERT INTO `pm_role_resource` VALUES (null, '1', '1', '31', '0', '2020-03-02 18:06:13', '2020-03-02 18:06:13');
