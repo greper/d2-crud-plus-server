c33026c5b3a8e8327d46835f5c13f15b5ed34b40
xiaojunnuo@126.com <xiaojunnuo@qq.com>
2020-09-01 17:21:29 +0800

    feat: 添加跨域支持

--- api-server/src/main/java/com/veryreader/d2p/api/web/interceptor/CORSInterceptor.java
+++ api-server/src/main/java/com/veryreader/d2p/api/web/interceptor/CORSInterceptor.java
@@ -12,7 +12,7 @@ public class CORSInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //添加跨域CORS
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,token");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,token,Authorization");
         response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
         return true;
     }
