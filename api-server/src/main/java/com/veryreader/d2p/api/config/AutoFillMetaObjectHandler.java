baa17f61e20c82948f268d014b9cc4ebedcf8743
xiaojunnuo@126.com <xiaojunnuo@qq.com>
2020-09-03 18:07:42 +0800

    fix: createtime 没有自动注入的问题

--- api-server/src/main/java/com/veryreader/d2p/api/config/AutoFillMetaObjectHandler.java
+++ api-server/src/main/java/com/veryreader/d2p/api/config/AutoFillMetaObjectHandler.java
@@ -17,25 +17,16 @@ public class AutoFillMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        log.debug("start insert fill ....");
-
-        LocalDateTime now = LocalDateTime.now();
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, now); // 起始版本 3.3.0(推荐使用)
-        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, now); // 起始版本 3.3.0(推荐使用)
-
-//        if(metaObject.hasGetter("delFlag") && metaObject.getValue("delFlag")==null){
-//            this.setFieldValByName("delFlag", "0", metaObject);//版本号3.0.6以及之前的版本
-//        }
-
-
-        //this.setInsertFieldValByName("operator", "Jerry", metaObject);//@since 快照：3.0.7.2-SNAPSHOT， @since 正式版暂未发布3.0.7
+        Date now = new Date();
+        this.strictInsertFill(metaObject, "createTime", Date.class, now);
+        this.strictInsertFill(metaObject, "updateTime", Date.class, now);
     }
 
 
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        log.debug("start update fill ....");
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+        Date now = new Date();
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, now);
     }
 }
